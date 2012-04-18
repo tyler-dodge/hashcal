fd0413de8db0ec9d932c11fdb8129a19d17c464e
Andy Joslin <andytjoslin@gmail.com>
2012-04-18 12:05:58 -0400

    added basic pickling and unpickling for list item

--- hashcal.py
+++ hashcal.py
@@ -5,12 +5,32 @@ Options:
   -v --verbose    print status messages
   -q --quiet      print only success or failure
   --version       print the current version
+  -a --add        add an item to the list
+  -s --show       show all items in the list
 """
 from docopt import docopt
+import pickle
 
-def main(options, arguments):
-    pass
+def main(options, args):
+	print args
+	print options
+	if options.add:
+		addItem(args)
+	if options.show:
+		printItems()
+
+
+def addItem(args):
+	f = file("test.dat", "wb")
+	pickle.Pickler(f).dump({ 'yourMom': args[0] })
+	f.close()
+
+
+def printItems():
+	f = file("test.dat", "rb")
+	print pickle.load(f)
+	f.close()
 
 if __name__ == '__main__':
-    options, arguments = docopt(__doc__, version="Version 0.1")
-    main(options,arguments)
+	options, args = docopt(__doc__)
+	main(options, args)
