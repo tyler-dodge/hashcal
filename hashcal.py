d2a20e09a26bde81356e536ff477baf9eeee5821
Tyler Dodge <thecitystate@gmail.com>
2012-04-18 10:54:12 -0400

    added version, and fixed mild typo

--- hashcal.py
+++ hashcal.py
@@ -2,8 +2,9 @@
 
 Options:
   -h --help       show this help message and exit
-  -h --verbose    print status messages
+  -v --verbose    print status messages
   -q --quiet      print only success or failure
+  --version       print the current version
 """
 from docopt import docopt
 
@@ -11,5 +12,5 @@ def main(options, arguments):
     pass
 
 if __name__ == '__main__':
-    options, arguments = docopt(__doc__)
+    options, arguments = docopt(__doc__, version="Version 0.1")
     main(options,arguments)
