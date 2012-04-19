5ee05969cba68f56731162f84f6532c9bb66ee84
Tyler Dodge <thecitystate@gmail.com>
2012-04-19 11:48:38 -0400

    made the storage path editable

--- hashcal.py
+++ hashcal.py
@@ -1,36 +1,40 @@
 """Usage: hashcal.py [options] arguments
 
 Options:
-  -h --help       show this help message and exit
-  -v --verbose    print status messages
-  -q --quiet      print only success or failure
-  --version       print the current version
-  -a --add        add an item to the list
-  -s --show       show all items in the list
+  -h --help         show this help message and exit
+  -v --verbose      print status messages
+  -q --quiet        print only success or failure
+  --version         print the current version
+  -a --add          add an item to the list
+  -s --show         show all items in the list
+  -f --file=<file>  file to store the calendar database in. [default: ~/.hashcal/cal.dat]
 """
 from docopt import docopt
 import pickle
-
+import os
 def main(options, args):
 	print args
 	print options
 	if options.add:
-		addItem(args)
+		addItem(options, args)
 	if options.show:
 		printItems()
 
 
-def addItem(args):
-	f = file("test.dat", "wb")
+def addItem(options, args):
+	if not os.path.exists(options.file):
+		os.makedirs(options.file[:options.file.rfind("/")])
+	f = file(options.file, "wb")
 	pickle.Pickler(f).dump({ 'yourMom': args[0] })
 	f.close()
 
 
 def printItems():
-	f = file("test.dat", "rb")
+	f = file(options.file, "rb")
 	print pickle.load(f)
 	f.close()
 
 if __name__ == '__main__':
 	options, args = docopt(__doc__)
+	options.file = os.path.expanduser(options.file)
 	main(options, args)
